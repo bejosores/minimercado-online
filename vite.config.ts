11cc86bbe6d970aeff2a2404f0b2cce3a12c761b
bernardoso <bernardo.soares@gavb.com.br>
2025-03-20 10:47:48 -0300

    delete

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/minimercado-online/', // <- Isso é essencial para GitHub Pages!
+  base: '/minimercado-online/',
 })
